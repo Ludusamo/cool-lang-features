7b3819d66e3aad0f3734ae904f021d47c00bf7fe
Brendan Horng <brendan.horng@ludusamo.com>
2018-10-01 22:17:04 -0400

    Add database create function

--- database/database.go
+++ database/database.go
@@ -3,3 +3,7 @@ package database
 type Database struct {
 	features map[string]*Feature
 }
+
+func CreateDatabase() *Database {
+    return &Database{make(map[string]*Feature)}
+}
---
+++ database/database_test.go
@@ -0,0 +1,12 @@
+package database
+
+import (
+	"testing"
+)
+
+func TestCreateDatabase(t *testing.T) {
+	db := CreateDatabase()
+    if db.features == nil {
+        t.Error("database features was not initialized")
+    }
+}
